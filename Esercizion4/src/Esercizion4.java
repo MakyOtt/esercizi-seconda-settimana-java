a197491da910d11577b4ee490f89f3947fccdd51
MakyOtt <marika.ottana20@gmail.com>
2023-04-19 22:35:46 +0200

    esercizio

---
+++ Esercizion4/src/Esercizion4.java
@@ -0,0 +1,72 @@
+
+import java.util.Arrays;
+import java.util.List;
+public class Esercizion4 {
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+    }
+
+    /**
+     * 1: Take the code below and put it into a function, read the code and see what it is.
+     * <p>
+     * What do you think the function does? Name the function appropriately and make sure
+     * <p>
+     * return the appropriate result type
+     *
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+        double celsius = 30;
+        System.out.println(temperatura(celsius));
+    }
+    private static String temperatura(double celsius){
+        double fahrenheit = ((celsius * 9) / 5) + 32;
+        String conversionDescription = celsius + " Celsius is " + fahrenheit + " fahrenheit";
+        return conversionDescription;
+    }
+
+    /**
+     * 2: Create a function that processes and prints out all the names in the ArrayList
+     *
+     *    Make sure to put all the formatting code in the function, formatting:
+     *    1) to Upper case
+     *    2) Remove spaces at the start and the end
+     *    3) If the name contains any numbers, instead of returning the name, return "Invalid"
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        List<String> names = Arrays.asList("Alan   ", "Alice   ", "Ant0ni0   ", "  Aziz");
+
+        for (String name : names) {
+            System.out.println(nomiFormat(name));
+        }
+    }
+    private static String nomiFormat(String nome){
+        char[] caratteri =nome.toCharArray();
+        for (char i : caratteri){
+            if(Character.isDigit(i))
+                return "invalid";
+        }
+        return nome.toUpperCase().trim();
+
+    }
+
+    /**
+     * 3: Write a function that prints out every variable seen in the code. It should not return anything
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3:");
+
+        double myLunchPrice = 5.99;
+        String lunchType = "sandwich";
+        String description = "Nice big sandwich";
+        int weightInGrams = 500;
+
+        stampa(myLunchPrice,lunchType,description,weightInGrams);
+    }
+    private static void stampa(double prezzo,String tipo,String descrizione,int grammi){
+        System.out.println("prezzo: "+prezzo+" tipo: "+tipo+" descrizione: "+descrizione+" grammi: "+grammi);
+    }
+}
